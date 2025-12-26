fc4188f6fe12cba1b549d23df6a47ec97a7e7671
Pawlooz <paweloski30@gmail.com>
2025-12-26 02:11:15 +0100

    Update computer.js

--- computer.js
+++ computer.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-function openFolder(name) {
-  if (name === "logs") {
-    window.location.href = "logs.html";
-    return;
-  }
-
-  if (name === "admin") {
-    if (localStorage.getItem("adminUnlocked") === "true") {
-      window.location.href = "admin.html";
-      return;
-    }
-
-    const pass = prompt("PASSWORD REQUIRED");
-
-    if (pass === "0417") {
-      localStorage.setItem("adminUnlocked", "true");
-      window.location.href = "admin.html";
-    } else {
-      alert("ACCESS DENIED");
-    }
-  }
-}
-=======
 function openFolder(name) {
   const passwordRequiredFolders = {
     "admin": "0417",
